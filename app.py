1c4280199d8802390c8b2c046d4055e408093cd4
alf4 <us82@gmx.de>
2020-04-29 00:03:05 +0200

    first code

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print('Hello world')
+# from selenium import webdriver
+
+# driver = webdriver.Firefox(r'C:\\Users\\udos8\\Downloads\\geckodriver.exe')
+# driver.get('https://www.ebay-kleinanzeigen.de/m-einloggen.html?targetUrl=/anzeigen/m-einloggen.html')
+
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+
+
+with webdriver.Firefox() as driver:
+    driver.get("http://google.com/ncr")
+    driver.find_element_by_name("q").send_keys("cheese" + Keys.RETURN)
+
+    wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>a")))
+
+    results = driver.find_elements_by_css_selector("h3>a")
+    for i, result in results.iteritems():
+        print("#{}: {} ({})".format(i, result.text, result.get_property("href")))
+
+wait = WebDriverWait(driver, 10)
